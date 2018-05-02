7858d548c47a888d20f0fc0f61a84eb24a6ce48f
Erik Plimmer <Elp013@Gmail.com>
2018-05-02 10:24:20 -0400

    added react-render-dom

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,18 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-      <h1>Welcome to PediaPedia!</h1>
-      <h3>-- Website currently under construction --</h3>
+        <div class="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
+
+<h3 class="content-subhead">
+          <i class="fa fa-heart"></i>
+          Purpose
+
+      </h3>
+<p>
+  Here at My Perfect Pet, we understand that pets make the world a brighter place. We also understand that having a pet that matches your personality and lifestyle is very important. There are millions of animals looking for their forever home, and our
+  goal is to match them with the perfect person!
+</p>
+</div>
       </div>
     );
   }
