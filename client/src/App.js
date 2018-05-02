77eaa2f59df27c04c4ee516de0eacf096ceb1107
Erik Plimmer <Elp013@Gmail.com>
2018-05-02 10:45:06 -0400

    fixed link errors on home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import Home from "./components/Home";
 import Baby from "./components/Baby";
-import Child from "./components/Child";
-import Toddlers from "./components/Toddlers";
+// import Child from "./components/Child";
+// import Toddlers from "./components/Toddlers";
 import './App.css';
 
 class App extends React.Component {
@@ -11,23 +11,8 @@ class App extends React.Component {
     return (
       <Router>
       <div className="App">
-<<<<<<< HEAD
-        <div class="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
-
-<h3 class="content-subhead">
-          <i class="fa fa-heart"></i>
-          Purpose
-
-      </h3>
-<p>
-  Here at My Perfect Pet, we understand that pets make the world a brighter place. We also understand that having a pet that matches your personality and lifestyle is very important. There are millions of animals looking for their forever home, and our
-  goal is to match them with the perfect person!
-</p>
-</div>
-=======
         <Route exact="exact" path="/" component={Home}/>
         <Route exact="exact" path="/Baby" component={Baby}/>
->>>>>>> cacff55237965b520fc6dd9ea2e15b20b5ae121e
       </div>
     </Router>
   );
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import "./pricing.css";
 
 class Home extends React.Component {
   render() {
     return (
       <div className="App"><div class="pure-menu pure-menu-horizontal">
-          <a href="#" class="pure-menu-heading">Your Logo</a>
+          <a href="/" class="pure-menu-heading">Your Logo</a>
           <ul class="pure-menu-list">
-              <li class="pure-menu-item"><a href="#" class="pure-menu-link">Home</a></li>
-              <li class="pure-menu-item pure-menu-selected"><a href="#" class="pure-menu-link">Pricing</a></li>
-              <li class="pure-menu-item"><a href="#" class="pure-menu-link">Contact</a></li>
+              <li class="pure-menu-item"><a href="/Baby" class="pure-menu-link">Home</a></li>
+              <li class="pure-menu-item pure-menu-selected"><a href="/Toddlers" class="pure-menu-link">Pricing</a></li>
+              <li class="pure-menu-item"><a href="/Child" class="pure-menu-link">Contact</a></li>
           </ul>
       </div>
 
