52f190ae0008b60ef25f5236ac0f10c822fd47e8
Erik Plimmer <Elp013@Gmail.com>
2018-05-02 19:53:25 -0400

    changed component folder structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from "react-router-dom";
-import Home from "./components/Home";
-import Baby from "./components/Baby";
+import Home from "./components/Home/Home";
+import Baby from "./components/Baby/Baby";
 // import Child from "./components/Child";
 // import Toddlers from "./components/Toddlers";
 import './App.css';
