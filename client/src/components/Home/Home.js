21a80f9d850015d1e9af3c5e30fd194d070873e4
Ashley Mackenzie <shannon.ashley.mackenzie@gmail.com>
2018-05-05 11:57:21 -0400

    minor change to text on home.js

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -99,7 +99,7 @@ class Home extends React.Component {
         <div class="information pure-g">
           <div class="pure-u-1 pure-u-md-1-2">
             <div class="l-box">
-              <h3 class="information-head">Get started today</h3>
+              <h3 class="information-head">Section Title</h3>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
               </p>
@@ -108,7 +108,7 @@ class Home extends React.Component {
 
           <div class="pure-u-1 pure-u-md-1-2">
             <div class="l-box">
-              <h3 class="information-head">Pay monthly or annually</h3>
+              <h3 class="information-head">Section 2 Title</h3>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse.
               </p>
@@ -117,7 +117,7 @@ class Home extends React.Component {
 
           <div class="pure-u-1 pure-u-md-1-2">
             <div class="l-box">
-              <h3 class="information-head">24/7 customer support</h3>
+              <h3 class="information-head">Section 3 Title</h3>
               <p>
                 Cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
               </p>
@@ -126,7 +126,7 @@ class Home extends React.Component {
 
           <div class="pure-u-1 pure-u-md-1-2">
             <div class="l-box">
-              <h3 class="information-head">Cancel your plan anytime</h3>
+              <h3 class="information-head">Section 4 Title</h3>
               <p>
                 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
               </p>
