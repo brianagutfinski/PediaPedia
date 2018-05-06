f506d4a87bbba23fd5bb48abe741c8cdb012eae2
rupamkumari24 <rupamkiran@gmail.com>
2018-05-06 16:35:52 -0400

    Login Implementation

--- server.js
+++ server.js
@@ -1,31 +1,61 @@
 const express = require("express");
+
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+
+//const mongoose = require("mongoose");
+
 // const APIRoutes = require("./routes/API");
-const bootstrap = require("bootstrap@3");
+
+//const bootstrap = require("bootstrap@3");
+
 const path = require("path");
+
 const PORT = process.env.PORT || 3001;
+
 const app = express();
 
+
+
 // Serve up static assets (usually on heroku)
+
 if (process.env.NODE_ENV === "production") {
+
   app.use(express.static("client/public"));
+
 }
 
+
+
 // Add API Routes
+
 // app.use("/api/article", APIRoutes);
 
+
+
 // Send every request to the React app
+
 // Define any API routes before this runs
 
 
+
+
+
 app.get("*", function(req, res) {
+
   res.sendFile(path.join(__dirname, "./client/public/index.html"));
+
 });
 
+
+
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/PediaPediaDB");
+
+//mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/PediaPediaDB");
+
+
 
 app.listen(PORT, function() {
+
   console.log(`🌎 ==> Server now on port ${PORT}!`);
+
 });
